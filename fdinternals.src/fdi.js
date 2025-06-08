c4a18c68e309731d3a96be7051fd4fef370e9aa8
ia74 <68617740+ia74@users.noreply.github.com>
2025-06-08 15:29:48 -0500

    update devutils and fdinternals

--- fdinternals.src/fdi.js
+++ fdinternals.src/fdi.js
@@ -1,10 +1,42 @@
-const {Plugin, HookRef} = require("@freedeck/api");
+const {Plugin, events, intents} = require("@freedeck/api");
+const connectRouter = require("@routers/connect");
+const sEvents = require("@handlers/eventNames");
+const { compileWebpack } = require("@src/webpack");
 
 class FDInternals extends Plugin {
     setup() {
+        this.requestIntent(intents.IO);
+        this.on(events.button, this.btn);
+        connectRouter.discoveryInformation.fdinternals = true;
+
         this.register({
-            display: 'Stop All',
+			display: "Reload All Connected Sockets",
+			type: "fdi.rl"
+		});
+		
+		this.register({
+			display: "Recompile Webpack Bundles",
+			type: "fdi.rc"
+		});
+
+        this.register({
+            display: 'Stop All Playing Sounds',
             type: 'fd.stopall'
+        })
+
+        this.register({
+            display: "Static Slider",
+            type: "fdi.es",
+            renderType: "slider",
+            templateData: {
+                min: 0, max: 100, value: 20, direction: "vertical", format: "format"
+            }
+        })
+
+        this.register({
+            display: "Static Text",
+            type: "fdi.et",
+            renderType: "text"
         })
 
         this.register({
@@ -15,6 +47,15 @@ class FDInternals extends Plugin {
         return true;
     }
 
+    btn({interaction, io}) {
+        if(interaction.type === 'fdi.rl') {
+            io.emit(sEvents.default.reload)
+        }
+        if(interaction.type === 'fdi.rc') {
+            io.emit(sEvents.default.recompile)
+            compileWebpack().catch((err) => console.error(err));
+        }
+    }
 }
 
 module.exports = {
