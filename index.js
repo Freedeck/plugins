108812a5b1d30fcc9f5d691945e016fdb1220f41
ia74 <aidenjs9@icloud.com>
2026-05-26 11:03:21 -0500

    - Update build system to use builder pattern, include more than one repository at once, with new naming schema  (TITLE.repo.json), AND a better favorite/push-to-top system

--- index.js
+++ index.js
@@ -1,51 +1,112 @@
-const {makePackage, Operations} = require('./src/lib/developerApi');
-const path = require('node:path');
-const fs = require('node:fs');
-require('module-alias/register');
-
-let mainRepository = {};
-let devRepo = {};
-
+const { makePackage, Operations } = require("./src/lib/developerApi");
+const path = require("node:path");
+const fs = require("node:fs");
+require("module-alias/register");
 console.log("INFO / STAGE 1 >> Building / validating all plugins");
+const defaultRepositoryName = "main";
+const repositories = {[defaultRepositoryName]:{}}
+const favorites = {};
 
-build("testpluginv2", [], true);
-build("Clock", []);
-build("MyExampleTheme", [], true);
-build("DevUtils", [], true);
-build("HAFreedeck", []);
-build("YTMD", []);
-build("Twitch", []);
-build("Kick", []);
-build("TextBG", []);
-build("DemoShowcase", []);
-build("dynamic-icons", [],true);
-build("Spotify", []);
-build("fdinternals",[], true);
-build("ExamplePlugin", [], true);
-build("WaveLink",[Operations.INSTALL_DEPS_PRE_PACKAGE]);
-build("myinstants",[Operations.INSTALL_DEPS_PRE_PACKAGE]);
-build("EasyMidi",[Operations.INSTALL_DEPS_PRE_PACKAGE]);
-build("OBSControl", [Operations.INSTALL_DEPS_PRE_PACKAGE]);
-build("StreamChatMonitor", [Operations.INSTALL_DEPS_PRE_PACKAGE]);
+/** Developer Repository */
+newPackage("fdinternals").isFavorite().setRepository("dev").build();
+newPackage("testpluginv2").setRepository("dev").build();
+newPackage("MyExampleTheme").setRepository("dev").build();
+newPackage("dynamic-icons").setRepository("dev").build();
+newPackage("ExamplePlugin").setRepository("dev").build();
+newPackage("DemoShowcase").setRepository("dev").build();
 
-function build(packageId, extra=[Operations.INSTALL_DEPS_PRE_PACKAGE], development = false) {
+/** Main Repository */
+newPackage("Spotify").isFavorite().build();
+newPackage("OBSControl").isFavorite().addExtra(Operations.INSTALL_DEPS_PRE_PACKAGE).build();
+newPackage("WaveLink").isFavorite().addExtra(Operations.INSTALL_DEPS_PRE_PACKAGE).build();
+newPackage("myinstants").isFavorite().addExtra(Operations.INSTALL_DEPS_PRE_PACKAGE).build();
+newPackage("Clock").isFavorite().build();
+newPackage("YTMD").isFavorite().build();
+newPackage("TextBG").isFavorite().build();
+newPackage("HAFreedeck").build();
+newPackage("Twitch").build();
+newPackage("Kick").build();
+newPackage("EasyMidi").addExtra(Operations.INSTALL_DEPS_PRE_PACKAGE).build();
+newPackage("StreamChatMonitor").addExtra(Operations.INSTALL_DEPS_PRE_PACKAGE).build();
+
+console.log("INFO / STAGE 2 >> Setting up favorites...");
+
+Object.keys(repositories).forEach((repo) => {
+  console.log("INFO / STAGE 2 >> Working on " + repo + " favorites..")
+
+  favorites[repo].forEach((packageId) => {
+    if (repositories[repo][packageId]) {
+      if(!repositories[repo+'sorted']) repositories[repo+'sorted'] = {};
+      repositories[repo+'sorted'][packageId] = repositories[repo][packageId];
+      delete repositories[repo][packageId];
+    }
+  });
+
+
+  Object.assign(repositories[repo+'sorted'], repositories[repo]);
+  repositories[repo] = repositories[repo+'sorted'];
+
+  fs.writeFileSync(
+    path.resolve(__dirname, repo+".repo.json"),
+    JSON.stringify(repositories[repo], null, 2),
+  );
+})
+
+console.log("DONE. Built repository index and packages.");
+
+function newPackage(source) {
+  const pkg = {
+    source,
+    extras: [],
+    repository: defaultRepositoryName,
+    favorite: false,
+    setSource: (t) => {
+      pkg.source = t;
+      return pkg;
+    },
+    addExtra: (t) => {
+      pkg.extras.push(t);
+      return pkg;
+    },
+    setRepository: (f) => {
+      pkg.repository = f;
+      if(!repositories[pkg.repository]) repositories[pkg.repository] = {};
+      return pkg;
+    },
+    isFavorite: () => {
+      pkg.favorite = true;
+      return pkg;
+    },
+    build: () => {
+      if(pkg.favorite) {
+        if(!favorites[pkg.repository]) favorites[pkg.repository] = [];
+        favorites[pkg.repository].push(pkg.source);
+      }
+      build(pkg.source, pkg.extras, pkg.repository);
+    },
+  };
+  return pkg;
+}
+
+function build(packageId, extra = [], repository = defaultRepositoryName) {
   makePackage({
     id: packageId,
     src: path.resolve(`${packageId}.src`),
     out: path.resolve("./plugins"),
-    extra: extra
+    extra: extra,
   });
 
-  const folderPath = path.resolve(__dirname, packageId +".src");
-  if(!fs.existsSync(folderPath)){
+  const folderPath = path.resolve(__dirname, packageId + ".src");
+  if (!fs.existsSync(folderPath)) {
     console.error(`ERROR >> Source directory ${folderPath} does not exist.`);
     process.exit(1);
   }
 
-  const {freedeck, name, author, description, version} = require(path.resolve(folderPath, "package.json"));
+  const { freedeck, name, author, description, version } = require(
+    path.resolve(folderPath, "package.json"),
+  );
 
-  const usedRepo = development ? devRepo : mainRepository;
-
+  const usedRepo = repositories[repository];
   usedRepo[name] = {
     message: freedeck.message,
     source: `github:freedeck/${name}`,
@@ -53,34 +114,6 @@ function build(packageId, extra=[Operations.INSTALL_DEPS_PRE_PACKAGE], developme
     title: freedeck.title,
     description,
     version,
-    download: `https://freedeck.github.io/plugins/plugins/${name}.fdpackage`
-  }
+    download: `https://freedeck.github.io/plugins/plugins/${name}.fdpackage`,
+  };
 }
-
-console.log("INFO / STAGE 2 >> Sorting Main Repository");
-
-const favoritedPackages = [
-  "spotify",
-  "obscontrol",
-  "wavelink",
-  "myinstants",
-  "clock",
-  "ytmd",
-  "textbg"
-]
-
-const sortedMainRepository = {};
-favoritedPackages.forEach(packageId => {
-  if(mainRepository[packageId]){
-    sortedMainRepository[packageId] = mainRepository[packageId];
-    delete mainRepository[packageId];
-  }
-});
-
-Object.assign(sortedMainRepository, mainRepository);
-mainRepository = sortedMainRepository;
-
-fs.writeFileSync(path.resolve(__dirname, "repository.json"), JSON.stringify(mainRepository, null, 2));
-fs.writeFileSync(path.resolve(__dirname, "development.json"), JSON.stringify(devRepo, null, 2));
-
-console.log("DONE. Built repository index and packages.")
